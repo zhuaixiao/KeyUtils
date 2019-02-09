2f4399d31869c3852831abf2d71954ce744d6fdd
hekai <1095859445@qq.com>
2019-02-09 13:24:20 +0800

    MD5/AES/RSA加密示例，AlignTextView左右对齐TextView"alignOnlyOneLine属性（boolean值，默认false）单行时是否平分铺满该行"

--- app/src/main/java/com/example/keyutils/MD5Utils.java
+++ app/src/main/java/com/example/keyutils/MD5Utils.java
@@ -1,5 +1,7 @@
 package com.example.keyutils;
 
+import android.util.Base64;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -15,16 +17,8 @@ public class MD5Utils {
             MessageDigest md5 = MessageDigest.getInstance("MD5");
             md5.update(info.getBytes("utf-8"));
             byte[] encryption = md5.digest();
-            StringBuffer stringBuffer = new StringBuffer();
-            for (int i = 0; i < encryption.length; i++) {
-                if (Integer.toHexString(0xff & encryption[i]).length() == 1) {
-                    stringBuffer.append("0").append(Integer.toHexString(0xff & encryption[i]));
-
-                } else {
-                    stringBuffer.append(Integer.toHexString(0xff & encryption[i]));
-                }
-            }
-            return stringBuffer.toString();
+            String result = Base64.encodeToString(encryption, Base64.DEFAULT);
+            return result;
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         } catch (UnsupportedEncodingException e) {
@@ -48,14 +42,8 @@ public class MD5Utils {
             while (dis.read(buffer) > 0) ;
             messageDigest = dis.getMessageDigest();
             byte[] array = messageDigest.digest();
-            StringBuffer hex = new StringBuffer(array.length * 2);
-            for (byte b : array) {
-                if ((b & 0xFF) < 0x10) {
-                    hex.append("0");
-                }
-                hex.append(Integer.toHexString(b & 0xFF));
-            }
-            return hex.toString();
+            String result = Base64.encodeToString(array, Base64.DEFAULT);
+            return result;
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         } catch (FileNotFoundException e) {
